e65b1a5d28dbcad445742ea5ce067ceee785229e
Falicia <ongfali@gmail.com>
2021-06-23 22:43:01 +0800

    Milestone 2

--- Final/app/src/main/java/com/example/orbital/CodingEvent.java
+++ Final/app/src/main/java/com/example/orbital/CodingEvent.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.graphics.Color;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class CodingEvent extends AppCompatActivity {
     private Button button_chat, button_join;
@@ -41,7 +44,23 @@ public class CodingEvent extends AppCompatActivity {
     }
 
     public void openJoined() {
-        Intent intent = new Intent(this, Joined.class);
+        Intent intent = new Intent(Intent.ACTION_INSERT);
+        intent.setType("vnd.android.cursor.item/event");
+
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2021,5,25,14,0);
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2021,5,25,17,0);
+
+
+        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
+        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,endTime.getTimeInMillis());
+
+        intent.putExtra(CalendarContract.Events.TITLE, "Coding Games for Kids");
+        intent.putExtra(CalendarContract.Events.DESCRIPTION,  "Volunteer with the kids!");
+        intent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Zoom");
+        //intent.putExtra(CalendarContract.Events.RRULE, "FREQ=YEARLY");
+
         startActivity(intent);
     }
 
